2990132c5c26da9ed9e5d72aa139b1234f3b570b
NadimRai <rai_nadim@yahoo.com>
2017-11-03 21:31:00 +0800

    added body-parser module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -15,12 +16,20 @@ mongoose.connect('mongodb://localhost/videojot-dev',{
 .then(() => console.log('MongoDB connected..'))
 .catch(err => console.log(err));
 
+//load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 //Handlebars Middleware
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 
 app.set('view engine', 'handlebars');
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
 //Index Route
 app.get('/', (req, res) =>{
@@ -34,6 +43,17 @@ app.get('/', (req, res) =>{
 app.get('/about', (req,res)=>{
     res.render('about');
 })
+
+//Idea Route
+app.get('/ideas/add', (req,res) =>{
+    res.render('ideas/add');
+})
+
+//Process Form
+app.post('/ideas', (req,res) =>{
+    console.log(req.body);
+    res.send('ok');
+});
 
 app.listen(port, () =>{
     console.log(`Server started on ${port}`);
