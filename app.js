5256f8cfce8d78a219fa2e9c84f6cd10f5da6fca
NadimRai <rai_nadim@yahoo.com>
2017-11-03 17:49:03 +0800

    created model Idea.js in model directory

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 5000;
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+//Connect to mongoose
+mongoose.connect('mongodb://localhost/videojot-dev',{
+    useMongoClien: true
+})
+.then(() => console.log('MongoDB connected..'))
+.catch(err => console.log(err));
 
 //Handlebars Middleware
 app.engine('handlebars', exphbs({
